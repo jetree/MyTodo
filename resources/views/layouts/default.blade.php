8e04a816e4e339b60cf2a47fb29140cee9c20e3c
神谷友樹 <jetree900@gmail.com>
2019-08-07 23:21:19 +0900

    analytics追加

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -26,6 +26,15 @@
     @if(app('env')=='production')
       <script src="{{ secure_asset('js/app.js') }}"></script>
       <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138358404-4"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-138358404-4');
+        </script>
     @endif
 
     <!-- Styles -->
