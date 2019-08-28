fdc4af7a8ce7a1b2551ece7e97e87792a91cd486
神谷友樹 <jetree900@gmail.com>
2019-08-28 23:41:06 +0900

    testuserlogin機能を実装

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,22 @@
 (function(){
   'use strict';
 
-  function $testlogin(user_name){
+  const email_area = document.getElementById('email')
+  const password_area = document.getElementById('password')
+
+  function $testlogin(user_name,email,password){
     let cmds = document.getElementById('login_' + user_name)
     console.log(cmds)
 
     cmds.addEventListener('click',function(e){
       e.preventDefault()
+      email_area.value = email
+      password_area.value = password
 
     })
   }
 
-  $testlogin("test1");
-  $testlogin("test2");
+  $testlogin("test1","test1@test.com","testtest");
+  $testlogin("test2","test2@test.com","testtest");
 
 })();
